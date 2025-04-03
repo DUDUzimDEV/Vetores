74bb833bb3aa4c3ba86008345cada3fa9cf0358a
DUDUzimDEV <eduardo.b.vieira8@aluno.senai.br>
2025-04-03 10:00:37 -0300

    Exercicio1

---
+++ main.go
@@ -0,0 +1,10 @@
+package main 
+ 
+ import "fmt"
+
+ func main () {
+	var numeros = []int{1, 2, 3, 4, 5}
+	fmt.Println(numeros)
+	numeros = append(numeros, 6, 7, 8)
+	fmt.Println(numeros, len(numeros), cap(numeros))
+ }
